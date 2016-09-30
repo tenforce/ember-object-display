30bb76564ff4fdc79d5f0872f26f083b802173d9
Nóra Szepes <nora.szepes@tenforce.com>
2016-09-30 14:13:04 +0200

    fixes for esize text area init
    - now it calculates the length of the text and sets the rows attribute on init
    -- only if the field contains text
    - removes the rows attribute on resizing

--- addon/mixins/resize-textarea.js
+++ addon/mixins/resize-textarea.js
@@ -1,22 +1,31 @@
 import Ember from 'ember';
 
+// this mixin is for resizing the textareas depending on the size of text inside the textarea
 export default Ember.Mixin.create({
     didInsertElement: function() {
         this._super();
         Ember.run.later(function() {
-            // implement this hook in your own subclasses and run your jQuery logic there
-
             Ember.$.each(Ember.$('textarea'), function() {
+              // this sets the 'rows' attribute depending on the text length inside the text area
+                var text = Ember.$('textarea').val(),
+                    // look for any "\n" occurences
+                    matches = text.match(/\n/g),
+                    breaks = matches ? matches.length : 2;
+                if (text) {
+                    Ember.$('textarea').attr('rows', breaks + 2);
+                }
+            });
+            Ember.$.each(Ember.$('textarea'), function() {
+                // this resizes the height of the textarea
+                // and removes the rows attribute, because it is only needed on init
                 var offset = this.offsetHeight - this.clientHeight;
-                console.log(this.offsetHeight);
-                console.log(this.clientHeight);
-
                 var resizeTextarea = function(el) {
                     Ember.$(el).css('height', 'auto').css('height', el.scrollHeight + offset);
                 };
                 Ember.$(this).on('keyup input', function() {
+                    Ember.$(this).removeAttr('rows');
                     resizeTextarea(this);
-                }).removeAttr('data-autoresize');
+                });
             });
         });
     }
