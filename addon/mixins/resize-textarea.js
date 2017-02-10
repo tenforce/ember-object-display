fbcdb7d6444a978cdca11f3012272a4eb421ec3f
vincent.goossens <vincent.goossens@tenforce.com>
2017-02-10 14:38:54 +0100

    Added ResizeTextareaMixin to concept-textarea

--- addon/mixins/resize-textarea.js
+++ addon/mixins/resize-textarea.js
@@ -5,16 +5,6 @@ export default Ember.Mixin.create({
     didInsertElement: function() {
         this._super();
         Ember.run.later(function() {
-            // Ember.$.each(Ember.$('textarea'), function() {
-            //   // this sets the 'rows' attribute depending on the text length inside the text area
-            //     var text = Ember.$('textarea').val(),
-            //         // look for any "\n" occurences
-            //         matches = text.match(/\n/g),
-            //         breaks = matches ? matches.length : 2;
-            //     if (text) {
-            //         Ember.$('textarea').attr('rows', breaks + 2);
-            //     }
-            // });
             Ember.$.each(Ember.$('textarea'), function() {
                 // this resizes the height of the textarea
                 // and removes the rows attribute, because it is only needed on init
